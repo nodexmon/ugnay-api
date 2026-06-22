88be61e4016f3737e5cfd8c45304baa44ae704d8
nodexmon <johnpauldanmachi@gmail.com>
2026-06-23 02:30:01 +0800

    Changed JwtService to AuthJwtService

--- src/auth/jwt/jwt.service.spec.ts
+++ src/auth/jwt/jwt.service.spec.ts
@@ -1,15 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { JwtService } from './jwt.service';
+import { AuthJwtService } from './jwt.service';
 
 describe('JwtService', () => {
-  let service: JwtService;
+  let service: AuthJwtService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [JwtService],
+      providers: [AuthJwtService],
     }).compile();
 
-    service = module.get<JwtService>(JwtService);
+    service = module.get<AuthJwtService>(AuthJwtService);
   });
 
   it('should be defined', () => {
