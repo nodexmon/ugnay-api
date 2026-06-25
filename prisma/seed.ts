88c6a521f0897c4d10918fda71e75196902ccf93
nodexmon <johnpauldanmachi@gmail.com>
2026-06-25 18:40:40 +0800

    Did some shits

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,9 +1,10 @@
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../src/generated/prisma/client';
+import "dotenv/config"
 
 const prisma = new PrismaClient({
   adapter: new PrismaPg({
-    connectionString: process.env.DATABASE_URL as string,
+    connectionString: process.env["DATABASE_URL"],
   }),
 });
 
