c8e80773c990f46d491db580ee17d8c8ef99ed55
nodexmon <johnpauldanmachi@gmail.com>
2026-07-17 16:40:05 +0800

    chore(main): add helmet and 1mb body size limit

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,18 @@ import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
 import { AllExceptionsFilter } from '@/common/filters/all-exceptions.filter';
 import { HttpExceptionFilter } from '@/common/filters/http-exception.filter';
+import { json, urlencoded } from 'express';
+import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bodyParser: false });
+
+  app.use(json({ limit: '1mb' }));
+  app.use(urlencoded({ extended: true, limit: '1mb' }));
+  app.use(helmet());
+  app.enableCors({ origin: process.env.CORS_ORIGIN ?? false });
 
   app.useLogger(app.get(Logger));
-
-  app.enableCors({ origin: process.env.CORS_ORIGIN ?? false });
 
   app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
 
